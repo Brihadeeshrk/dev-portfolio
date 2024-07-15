eb91df92255390ab4fcfc055b4cd186162f13a77
briha <hello@briha.xyz>
2024-07-15 22:14:38 +0530

    under dev

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,9 @@
-import { BlogPosts } from 'app/components/posts'
+import { BlogPosts } from "app/components/posts";
 
 export default function Page() {
   return (
     <section>
-      <h1 className="mb-8 text-2xl font-semibold tracking-tighter">
+      {/* <h1 className="mb-8 text-2xl font-semibold tracking-tighter">
         My Portfolio
       </h1>
       <p className="mb-4">
@@ -15,7 +15,8 @@ export default function Page() {
       </p>
       <div className="my-8">
         <BlogPosts />
-      </div>
+      </div> */}
+      <p className="italic font-light">under maintenance</p>
     </section>
-  )
+  );
 }
